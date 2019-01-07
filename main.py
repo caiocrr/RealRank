61bcfe7437d26202a49dfc6e6160f4d1d15dd76b
Caio Ramos <caiotecn@yahoo.com.br>
2019-01-07 00:40:01 -0200

    chamadas de teste

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Created on Tue Dec 25 00:45:31 2018
 import sys
 sys.path.append("struc2vec/src")
 import s2v
+import algorithms_distances as ad
 class args: 
     input = "data/steam.edgelist";
     output = "output/steam.struc2vec";
@@ -28,7 +29,18 @@ class args:
     
 
 def main():
-    s2v.execs2v(args)
+    #s2v.execs2v(args)
+    # weights_distances_r = ad.restoreVariableFromDisk('distances-r-'+str(1))
+    # s2v.logging.info('{}'.format(weights_distances_r))
+
+    G = s2v.read_graph(args)
+    G = s2v.struc2vec.Graph(G, args.directed, args.workers, untilLayer = None)
+    G.calc_distances(compactDegree = args.OPT1)
+    # G.create_distances_network()
+    # G.preprocess_parameters_random_walk()
+    # G.simulate_walks(args.num_walks, args.walk_length)
+
+
     # G = s2v.read_graph(args)
     # G = s2v.struc2vec.Graph(G, args.directed, args.workers, untilLayer = None)
     # G.simulate_walks(args.num_walks, args.walk_length)
@@ -36,7 +48,7 @@ def main():
     # G = s2v.struc2vec.Graph(G, args.directed, args.workers, untilLayer = args.until_layer)
     # G.preprocess_neighbors_with_bfs_compact()
 
-    #s2v.learn_embeddings(args)
+    # s2v.learn_embeddings(args)
 
 if __name__ == '__main__':
     main()
