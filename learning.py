b89b51a7a1b1a27e8e788643dd3c9e2a696d1f52
Caio Ramos <caiotecn@yahoo.com.br>
2019-01-19 16:17:20 -0200

    Embedding e config df_train

--- learning.py
+++ learning.py
@@ -7,8 +7,9 @@ import operator
 def set_classification(input, train):
         df = pd.read_csv(input, skiprows=1, sep=' ', header=None, index_col=0).sort_index()
         df_train = pd.read_csv(train, sep=' ', header=None, index_col=0).sort_index()
-        df_train = pd.concat([df_train, df], axis=1, join='inner')
-        print(df.head())
+        df_train.columns = ['confiavel']
+        df_train = pd.concat([df, df_train], axis=1, join='inner')
+        print(df_train.head(10))
 
 def sybil_rank():
         return
